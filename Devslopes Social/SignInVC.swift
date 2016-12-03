b38af41e961709013139e502ef9cc6fe626ff7aa
salferrari <sal@hardrocknetwork.com>
2016-12-02 18:40:40 -0800

    completed firebase setup integration

---
+++ Devslopes Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Devslopes Social
+//
+//  Created by rush on 12/2/16.
+//  Copyright © 2016 VOOCHIE. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Devslopes Social/Pods-Devslopes Social-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Devslopes_SocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Devslopes_SocialVersionString[];
+
